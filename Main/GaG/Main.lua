c12cf718ca5331763e43fb622dd7ceee48116965
JodelscriptHubPh <222961571+JodelscriptHubPh@users.noreply.github.com>
2025-07-31 07:42:05 +0800

    Delete Main/GaG directory

--- Main/GaG/Main.lua
+++
@@ -1,6 +0,0 @@
-local player = game.Players.LocalPlayer
-local inventory = player.Backpack
-for _, item in pairs(inventory:GetChildren()) do
-    local duplicate = item:Clone()
-    duplicate.Parent = inventory
-end
